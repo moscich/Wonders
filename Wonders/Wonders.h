894210f5935f2071dd5316e69659a4848f71f5ab
Marek Mościchowski <marek.moscichowski@codewise.com>
2018-07-28 17:58:48 +0200

    Initial Commit

---
+++ Wonders/Wonders.h
@@ -0,0 +1,19 @@
+//
+//  Wonders.h
+//  Wonders
+//
+//  Created by Marek Mościchowski on 28/07/2018.
+//  Copyright © 2018 Marek Mościchowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wonders.
+FOUNDATION_EXPORT double WondersVersionNumber;
+
+//! Project version string for Wonders.
+FOUNDATION_EXPORT const unsigned char WondersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wonders/PublicHeader.h>
+
+
